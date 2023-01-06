29c22b38978052a949ce8d6b642989468ea8b769
OrlundoHubbard <88997637+OrlundoHubbard@users.noreply.github.com>
2023-01-06 09:32:13 -0600

    Add files via upload

---
+++ api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+
+app.use(logger('tiny'));
+app.use(bodyParser.json());
